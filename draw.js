976e92832ac5c0415e020af6a095add558ed54e3
Zack Stout <zackstout@gmail.com>
2018-02-07 17:18:45 -0600

    added focus

--- draw.js
+++ draw.js
@@ -13,17 +13,19 @@ function draw() {
 // y goes from 0 to 800, or from -0.5 to 7.5
 function drawParabola(a, s) {
   translate(w/2, w/16);
-  // rect(0, 100, 50, 50);
-  // fill('blue');
 
   for (var i=0; i < s; i++) {
     var inc = w / s;
     var xPos = -4 + i * inc/100;
     var xNext = - 4 + (i + 1) * inc/100;
-    var yPos = xPos * xPos;
-    var yNext = xNext * xNext;
+    var yPos = a * xPos * xPos;
+    var yNext = a * xNext * xNext;
     line(xPos * 100, yPos * 100, xNext * 100, yNext * 100);
   }
+
+  var p = 1 / (4 * a);
+
+  ellipse(0, p * 100, 20, 20);
 
   translate(-w/2, -w/16);
 }
