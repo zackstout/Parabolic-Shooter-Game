ed8d7a058bc9ca36c4e728a92bc7b33bcdaee904
Zack Stout <zackstout@gmail.com>
2018-02-07 17:52:08 -0600

    balls now shooting in direction of mouseclick, with force proportional to distance from focus

--- draw.js
+++ draw.js
@@ -13,12 +13,15 @@ var Mouse = Matter.Mouse;
 var world, engine;
 
 
+
 function setup() {
   var can = createCanvas(w, w);
   background(200);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
+
+  world.gravity.y = 0;
 
   var mouse = Mouse.create(can.elt);
   mouse.pixelRatio = pixelDensity();
@@ -28,9 +31,13 @@ function setup() {
 
 function draw() {
   background(200);
-  drawParabola(1/3, 200);
+  drawParabola(1/5, 200);
   for (var i=0; i < allBalls.length; i++) {
+    // fill(getRandomColor());
     ellipse(allBalls[i].position.x, allBalls[i].position.y, 10, 10);
+    if (allBalls[i].position.x > 500) {
+      Body.setVelocity(allBalls[i], {x: 0, y: 10});
+    }
   }
 }
 
@@ -38,12 +45,27 @@ function mouseClicked() {
   // translate(w/2, w/16);
   console.log('clickin', mouseX, mouseY);
   // why on earth do we have to add 100 rather than 50 here?
-  var newBall = Bodies.circle(w / 2, focus * 100 + 100, 10);
+  var newBall = Bodies.circle(w / 2, focus + 50, 10);
   allBalls.push(newBall);
   World.add(world, newBall);
-  Body.applyForce(newBall, {x: w / 2, y: focus * 100 + 100}, {x:0.03, y: 0.01});
 
+  var xComp = (mouseX - w / 2)/100000;
+  var yComp = (mouseY - (focus + 50))/100000;
+
+  Body.applyForce(newBall, {x: w / 2, y: focus + 50}, {x: xComp, y: yComp});
+
+
+  console.log(xComp, yComp);
   // translate(-w/2, -w/16);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 
@@ -63,9 +85,9 @@ function drawParabola(a, s) {
 
   // draw focus:
   var p = 1 / (4 * a);
-  focus = p;
+  focus = p * 100 + 50;
   // don't forget to add the 50:
-  ellipse(0, 50 + p * 100, 7, 7);
+  ellipse(0, focus, 7, 7);
 
   translate(-w/2, -w/16);
 }
