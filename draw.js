ec605f5db78612d96dbee714f017fceee84e2a2d
Zack Stout <zackstout@gmail.com>
2018-02-07 17:09:25 -0600

    parabola almost working

--- draw.js
+++ draw.js
@@ -1,9 +1,29 @@
+var w = 800;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(w, w);
   background(200);
 }
 
 function draw() {
+  drawParabola(1, 20);
+}
 
+// go from x=0 to x=800, or from -4 to 4.
+// y goes from 0 to 800, or from -0.5 to 7.5
+function drawParabola(a, s) {
+  translate(w/2, w/16);
+  // rect(0, 100, 50, 50);
+  // fill('blue');
+
+  for (var i=0; i < s; i++) {
+    var inc = w / s;
+    var xPos = -4 + i * inc/100;
+    var xNext = - 4 + (i + 1) * inc/100;
+    var yPos = xPos * xPos;
+    var yNext = xNext * xNext;
+    line(xPos * 100, yPos * 10000, xNext * 100, yNext * 10000);
+  }
+
+  translate(-w/2, -w/16);
 }
